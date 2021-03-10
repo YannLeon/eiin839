1c82daf673eebe979dcec5e4bcdfc6beae459622
YannLeon <54958888+YannLeon@users.noreply.github.com>
2021-03-10 15:33:13 +0100

    soap

---
+++ webFront/SOAP/Soap web service client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soap_web_service_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator.CalculatorSoap c = new calculator.CalculatorSoapClient() ;
+            Console.WriteLine(c.Add(11, 11)) ;
+            Console.ReadLine();
+        }
+    }
+}
