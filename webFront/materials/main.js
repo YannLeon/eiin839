68ed1769c00b5d646c53ae791e18281b4386e3e1
YannLeon <54958888+YannLeon@users.noreply.github.com>
2021-03-13 11:42:10 +0100

    Td webfront ex 2

--- webFront/materials/main.js
+++ webFront/materials/main.js
@@ -1,1 +1,21 @@
-document.getElementById("workingTest").innerHTML="It works";
+const apikey = "330d91f01657af3c5749eccdf042fcad37d9c7c2";
+
+function retrieveAllContracts(){
+    var txtFromInput = document.getElementById("ntm")
+    if( txtFromInput==null ){
+        txtFromInput=apikey;
+    }
+    console.log("apikey = "+txtFromInput);
+
+    var callTxt="https://api.jcdecaux.com/vls/v1/stations?&apiKey=" + txtFromInput;
+    var request=new XMLHttpRequest();
+    request.open("GET",callTxt);
+    request.setRequestHeader("Accept","application/json");
+    request.onload=contractsRetrieved;
+    request.send();
+
+}
+
+function contractsRetrieved(){
+    console.log(JSON.parse(this.responseText))
+}
